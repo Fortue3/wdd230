32e353677725e1ecf82f9f20b15a66549954daf2
Fortue3 <kwe22002@byui.edu>
2023-09-29 07:59:06 +0200

    commit

---
+++ scripts/hamburger.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navbarMenu = document.getElementById("navbarMenu");
+
+    if (navbarMenu.style.display === "none") {
+        navbarMenu.style.display = "flex";
+    } else {
+        navbarMenu.style.display = "none";
+    }
+}
