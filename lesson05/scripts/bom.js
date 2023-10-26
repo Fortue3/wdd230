efd83005d7db6664e29e59c63b4caa556ebbafc8
Fortue3 <kwe22002@byui.edu>
2023-10-26 16:40:26 +0200

    Page visits

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,19 +3,34 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 var listCount = 0;
 
-button.addEventListener('click', () => {
+let chaptersArray = getChapterList() || [];
 
+    chaptersArray.forEach(chapter => {
+        displayList(chapter);
+        console.log(chapter)
+      });  
 
-
-    if (input.value != '') {
-        input.focus();
-        if (listCount < 10) {
-
-            const li = document.createElement('li');
+      function refresh() {
+        chaptersArray.forEach(chapter => {
+            displayList(chapter);
+            console.log(chapter)
+          });
+      }
+function deleteChapter(chapter) {
+        chapter = chapter.slice(0, chapter.length - 1);
+        chaptersArray = chaptersArray.filter(item => item !== chapter);
+        setChapterList();
+      }
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+  }
+async function displayList(item)
+{
+    const li = document.createElement('li');
             const deleteButton = document.createElement('button');
 
 
-            li.textContent = input.value;
+            li.textContent = await item;
             deleteButton.textContent = '✖️';
 
 
@@ -38,10 +53,35 @@ button.addEventListener('click', () => {
 
 
             input.value = '';
-        }
-        else {
-            window.alert("The maximum number of elements is 10, Please remove one from the list, to add a new one");
-        }
-    }
+}
 
-});
+button.addEventListener('click', () => {
+    if (input.value != '') {  // make sure the input is not empty  
+        displayList(input.value); // call the function that outputs the submitted chapter
+      chaptersArray.push(input.value);  // add the chapter to the array
+      setChapterList(); // update the localStorage with the new array
+      input.value = ''; // clear the input
+      input.focus(); // set the focus back to the input
+      //displayList(input.value); // call the function that outputs the submitted chapter
+    }
+  });
+   function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
+
+// button.addEventListener('click', () => {
+
+
+
+//     if (input.value != '') {
+//         input.focus();
+//         if (listCount < 10) {
+
+            
+//         }
+//         else {
+//             window.alert("The maximum number of elements is 10, Please remove one from the list, to add a new one");
+//         }
+//     }
+
+// });
